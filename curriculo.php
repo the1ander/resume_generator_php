4228380f9737ef539859c7bd70797402f54621a7
Anderson <andr.poltronieri@gmail.com>
2024-05-22 15:26:34 -0300

    first_commit_telaInicial_v1.0

---
+++ curriculo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>curriculo</title>
+    
+   
+    <script src="script\script.js"></script>
+</head>
+<h1>bem vindo ao curriculo</h1>
+
+<body>
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Menu inicial</title>
+    <link rel="stylesheet" href="/style/style.css">
+    <link href="/curriculo.php">
+    <link href="/proj.php">
+    <link href="/script/script.js">
+</head>
+
+<body>
+    <main class="sectionAll">
+
+        <section class="meuMenu">
+            <div class="topText">
+                <h1>Bem vindo ao gerador de currículo online</h1>
+            </div>
+            <div class="divMenuCurriculo">
+                <button id="btnCurriculo">
+                    <nav class="geradorCurriculo">
+                        <a class="curric" href="/curriculo.php">gerador de currículos</a>
+                    </nav>
+                </button>
+            </div>
+            <div class="divMeuProjeto">
+                <button id="btnProjeto">
+                    <nav class="geradorProjeto">
+                        <a class="proj" href="/proj.php">Documentação do projeto</a>
+                    </nav>
+                </button>
+            </div>
+
+            <div class="imgUnipar">
+                <img src="https://www.unipar.br/static/img/logos/horizontal.8265e340f954.svg" alt="logo_unipar" />
+            </div>
+
+            <div class=" lowText">
+                <h3>Trabalho realizado por Anderson Poltronieri da Luz.</h3>
+            </div>
+
+        </section>
+        
+    </main>
+
+</body>
